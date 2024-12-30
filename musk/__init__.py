c9fd4cff6a2ebca4cfcf6b6b933699373eedbd0b
jinxixiang <xiang_jinxi@qq.com>
2024-12-30 10:47:24 -0800

    update header

---
+++ musk/__init__.py
@@ -0,0 +1,8 @@
+# --------------------------------------------------------
+# MUSK: A Vision-Language Foundation Model for Precision Oncology
+# Published in Nature, 2025
+# GitHub Repository: https://github.com/lilab-stanford/MUSK
+# Copyright (c) 2025 Stanford University, by Jinxi Xiang
+# Licensed under the CC-BY-NC-ND 4.0 License (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+# Please see LICENSE for additional details.
+# --------------------------------------------------------
--- musk/modeling.py
+++ musk/modeling.py
@@ -1,4 +1,11 @@
-# MUSK utilizes a multimodal transformer backbone, implemented using torchscale https://github.com/microsoft/torchscale).
+# --------------------------------------------------------
+# MUSK: A Vision-Language Foundation Model for Precision Oncology
+# Published in Nature, 2025
+# GitHub Repository: https://github.com/lilab-stanford/MUSK
+# Copyright (c) 2025 Stanford University, by Jinxi Xiang
+# Licensed under the CC-BY-NC-ND 4.0 License (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+# Please see LICENSE for additional details.
+# --------------------------------------------------------
 
 import torch
 import torch.nn as nn
